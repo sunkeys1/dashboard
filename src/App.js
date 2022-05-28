19e393886e88ddfd5d756e5477ae24fc723a1624
sunkeys1 <basatax5@gmail.com>
2022-05-28 21:41:18 +0700

    setup appjs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
 
 const App = () => {
+  const activeMenu = false;
   return (
-    <h1 className='underline text-3xl'>App</h1>
+    <div>
+      <BrowserRouter>
+        <div className='flex relative dark:bg-main-dark-bg'>
+          <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{ background: 'gray', borderRadius: '50%'}}>
+                <FiSettings/>
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+              sidebar
+            </div>
+          ) : (
+            <div className='w-0 dark:bg-secondary-dark-bg'>
+              sidebar w-0
+            </div>
+          )}
+          <div className={
+            `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`
+          }>
+            <div className='fixed static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+              navbar
+            </div>
+          </div>
+          <div>
+            <Routes>
+              <Route path='/' element="EComerce" />
+              <Route path='/ecomerce' element="EComerce" />
+              
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
